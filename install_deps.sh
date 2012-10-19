1bc39f6c5c72a0b455047120fa80184bb10363c3
Tommy Odom <odom@finelineprototyping.com>
2012-10-19 09:31:09 -0400

    Added some extras from Paul Irish's repo

---
+++ install_deps.sh
@@ -0,0 +1,8 @@
+# https://github.com/rupa/z
+# z, oh how i love you
+mkdir -p ~/bin/z
+curl https://raw.github.com/rupa/z/master/z.sh > ~/bin/z/z.sh
+chmod +x ~/bin/z/z.sh
+
+sudo easy_install Pygments
+
