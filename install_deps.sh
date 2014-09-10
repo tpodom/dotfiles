01175bc6a628b9c2b04c2d113fcd03096936344f
Tommy Odom <tommy.odom@protolabs.com>
2014-09-10 10:51:25 -0400

    Updated to point at new github url for raw content

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,11 @@
 # https://github.com/rupa/z
 # z, oh how i love you
 mkdir -p ~/bin/z
-curl https://raw.github.com/rupa/z/master/z.sh > ~/bin/z/z.sh
+curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/bin/z/z.sh
 chmod +x ~/bin/z/z.sh
 
 sudo easy_install Pygments
 mkdir -p ~/.bash_completion.d
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > ~/.bash_completion.d/git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.bash_completion.d/git-completion.bash
+curl https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash > ~/.bash_completion.d/git-flow-completion.bash
 
